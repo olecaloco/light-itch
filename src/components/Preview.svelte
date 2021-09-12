21f92901d46788ac6dfc97c606b0f0d3a9ba5bf2
edward-altumo <jed.mac@altumo.com>
2021-09-12 12:16:29 +0800

    Added mini get started

--- src/components/Preview.svelte
+++ src/components/Preview.svelte
@@ -2,15 +2,39 @@
   export let source;
 </script>
 
-<iframe
-  src={source}
-  scrolling="no"
-  frameborder="0"
-  allowfullscreen
-  title="Player"
-/>
+{#if !source}
+  <div>
+    <h4>
+      Get started by typing the username of your twitch streamer in the field
+      above. It can be found on the url e.g. "twitch.tv/forsen"
+    </h4>
+  </div>
+{:else}
+  <iframe
+    src={source}
+    scrolling="no"
+    frameborder="0"
+    allowfullscreen
+    title="Player"
+  />
+{/if}
 
 <style>
+  div {
+    padding: 0 15px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h4 {
+    width: 300px;
+    max-width: 100%;
+    line-height: 1.5;
+  }
+
   iframe {
     flex: 1 1 0%;
     height: 100%;
